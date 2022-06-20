d5767cf3754155feff817f8311ff70de4457a4c5
David Ho <davidho0403@gmail.com>
2022-06-21 01:38:52 +0800

    Update js file

--- manage/manageStore.php
+++ manage/manageStore.php
@@ -49,7 +49,7 @@ if (!isset($_SESSION['employee_ssn'])) {
         <!-- Bootstrap JS -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <!-- Custom JS -->
-        <script src="../script/manage/manageStore.js"></script>
+        <script src="../script/manage/manageStore.js?2022062101"></script>
     </body>
 
 <?php
