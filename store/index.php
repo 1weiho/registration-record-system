8057f92295c431038af5b206ff0f0b3170251e50
David Ho <davidho0403@gmail.com>
2022-06-21 01:36:50 +0800

    Make text in tables smaller

--- store/index.php
+++ store/index.php
@@ -21,7 +21,7 @@ if (!isset($_SESSION['store_id'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-        <link rel="stylesheet" href="../style/store/store.css">
+        <link rel="stylesheet" href="../style/store/store.css?2022062101">
         <link rel="shortcut icon" href="../src/image/ico.png" type="image/x-icon">
         <title>店家後台系統 | 主頁</title>
     </head>
