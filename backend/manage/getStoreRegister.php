7008e83478564ad80ca18f390d239d7fd1bb5ec7
David Ho <davidho0403@gmail.com>
2022-06-21 23:17:33 +0800

    feat: add "register_count" & "average_count" index
    將回傳結果切分為三個索引，報名資料、報名總數、報名平均數

--- backend/manage/getStoreRegister.php
+++ backend/manage/getStoreRegister.php
@@ -15,11 +15,17 @@ if (isset($_POST["store_id"])) {
     $checkIdentity = $model->execute($sql);
     if ($checkIdentity) {
         $sql = "SELECT register_id, parent_name, phone_num, register_time FROM register_record NATURAL JOIN store_list WHERE store_id = {$store_id};";
-        $result = $model->execute($sql);
-        $count = count($result);
+        $result["data"] = $model->execute($sql);
+        $count = count($result["data"]);
+        $result["register_count"] = $count;
         for ($i = 0; $i < $count; $i++) {
-            $result[$i]['serial_num'] = $i + 1;
+            $result["data"][$i]['serial_num'] = $i + 1;
         }
+        $sql = $model->select("store_account", ["create_date"]) . $model->where("store_id", "=", $store_id);
+        $create_date = $model->execute($sql)[0]["create_date"];
+        $daydiff = floor((abs(strtotime(date("Y-m-d")) - strtotime($create_date)) / (60 * 60 * 24))) + 1;
+        $average_count = round(($count / $daydiff), 2);
+        $result["average_count"] = $average_count;
     } else {
         $result = false;
     }
